7cc975a576042ccbd6ac4213e8fb85a3f7bd8338
saikumarpinisetti3 <132870970+saikumarpinisetti3@users.noreply.github.com>
2023-07-10 12:44:15 +0530

    Update main.tf

--- modules/aws_natgw/main.tf
+++ modules/aws_natgw/main.tf
@@ -1,13 +1,5 @@
-resource "aws_nat_gateway" "example" {
-  allocation_id = var.epi_id
+resource "aws_nat_gateway" "natw" {
+  allocation_id = var.eip_id
   subnet_id     = var.subnet_id
 
 }
-
-output "natgw_id" {
-  value = aws_nat_gateway.example.id
-}
-
-
-# Lets launch ec2 instances into public and private subnets
-
